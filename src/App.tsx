82a651c6c30813c25fb3537edf6e9dc86719d145
OhRaekyu <dhforb1234@naver.com>
2022-07-23 23:44:50 +0900

    :recycle: 레포 재활용

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
 import './App.css';
 
-import MainPage from './pages/MainPage';
-
 function App() {
-  return (
-    <div className="app">
-      <MainPage />
-    </div>
-  );
+  return <div className="app"></div>;
 }
 
 export default App;
